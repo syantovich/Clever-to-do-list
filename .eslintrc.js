babd0b93ec34155cd6012fe966a8f85c5312a309
AlexeySyantovich <syantovich.a.s@gmail.com>
2022-08-02 11:15:33 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'prettier',
+    // 'plugin:import/errors',
+    // 'plugin:import/warnings',
+    // 'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react','prettier', '@typescript-eslint'],
+  rules: {
+    // 'react/jsx-filename-extension': [
+    //   2,
+    //   { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    // ],
+    'prettier/prettier': ['error'],
+  },
+};
