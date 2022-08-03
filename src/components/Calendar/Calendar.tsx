ba61e7e7aff14ea7fbb2b1866b2086887b5a8b91
AlexeySyantovich <syantovich.a.s@gmail.com>
2022-08-03 18:08:23 +0300

    calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Stack } from '@mui/material';
 import { currDayInMonth, dayInWeek, daysInMonth } from '../../constants';
-import CalendarDay from '../CalendarDay/CalendarDay';
-import AliceCarousel from 'react-alice-carousel';
+// import CalendarDay from '../CalendarDay/CalendarDay';
+// import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 const Calendar = () => {
